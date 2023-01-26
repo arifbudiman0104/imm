b89ca0224b1b88274dfb32c44caecf6fe0f1369d
arifbudimanar <arifbudiman01041996@hotmail.com>
2023-01-26 21:24:57 +0700

    fix header shadow

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -65,7 +65,7 @@
 
         <!-- Page Heading -->
         @if (isset($header))
-        <header class="bg-white shadow dark:bg-gray-800">
+        <header class="bg-white dark:bg-gray-800">
             <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                 {{ $header }}
             </div>
