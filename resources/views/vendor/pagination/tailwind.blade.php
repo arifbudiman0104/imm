ccc2c8e453a96d6235e94970cfddaf3c453415d5
arifbudimanar <arifbudiman01041996@hotmail.com>
2023-01-27 21:26:59 +0700

    fix pagination color light

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -3,21 +3,21 @@
         {{-- sm --}}
         <div class="flex justify-between flex-1 lg:hidden">
             @if ($paginator->onFirstPage())
-                <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-200 bg-white rounded-md cursor-default dark:text-gray-600 dark:bg-gray-700">
+                <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-200 bg-gray-100 rounded-md cursor-default dark:text-gray-600 dark:bg-gray-700">
                     {!! __('pagination.previous') !!}
                 </span>
             @else
-                <a href="{{ $paginator->previousPageUrl() }}" class="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white rounded-md dark:text-gray-200 dark:bg-gray-700 hover:text-gray-500 focus:outline-none active:bg-gray-100 active:text-gray-700">
+                <a href="{{ $paginator->previousPageUrl() }}" class="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-gray-100 rounded-md dark:text-gray-200 dark:bg-gray-700 hover:text-gray-500 focus:outline-none active:bg-gray-100 active:text-gray-700">
                     {!! __('pagination.previous') !!}
                 </a>
             @endif
 
             @if ($paginator->hasMorePages())
-                <a href="{{ $paginator->nextPageUrl() }}" class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white rounded-md dark:text-gray-200 dark:bg-gray-700 hover:text-gray-500 focus:outline-none active:bg-gray-100 active:text-gray-700">
+                <a href="{{ $paginator->nextPageUrl() }}" class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-gray-100 rounded-md dark:text-gray-200 dark:bg-gray-700 hover:text-gray-500 focus:outline-none active:bg-gray-100 active:text-gray-700">
                     {!! __('pagination.next') !!}
                 </a>
             @else
-                <span class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium leading-5 text-gray-200 bg-white rounded-md cursor-default dark:text-gray-600 dark:bg-gray-700">
+                <span class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium leading-5 text-gray-200 bg-gray-100 rounded-md cursor-default dark:text-gray-600 dark:bg-gray-700">
                     {!! __('pagination.next') !!}
                 </span>
             @endif
