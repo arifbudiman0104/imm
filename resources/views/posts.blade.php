6f019f45dff37997efa49cfed00956e8b603fc69
arifbudimanar <arifbudiman01041996@hotmail.com>
2023-02-05 01:22:30 +0700

    change post flex lg

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -54,7 +54,7 @@
                             @endforeach
                         </div>
                     </div>
-                    <div class="grid grid-cols-1 gap-5">
+                    <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-1">
                         @forelse ($posts as $post)
                         <x-card.post-public>
                             <a href="{{ route('post', $post->slug) }}">
@@ -91,8 +91,7 @@
                                         @endif
                                     </div>
                                     <div class="flex flex-col">
-                                        <div class="flex flex-col justify-between sm:flex-row">
-                                            {{-- 2xl:flex-col --}}
+                                        <div class="flex flex-col justify-between sm:flex-row md:flex-col lg:flex-row">
                                             <div class="flex items-center gap-1 mt-1">
                                                 <p class="text-gray-900 text-md dark:text-gray-100">
                                                     {{ $post->user->name }}
@@ -169,73 +168,6 @@
                 </div>
 
             </div>
-            {{-- <div class="grid grid-cols-1 gap-5 md:grid-cols-2 2xl:grid-cols-3">
-                @forelse ($posts as $post)
-                <x-card.post-public>
-                    <a href="{{ route('post', $post->slug) }}">
-                        <div class="flex flex-col justify-between h-full">
-                            <div class="flex flex-col">
-                                @if ($post->post_category_id != 1 || $post->is_featured == false)
-                                <div class="flex justify-between">
-                                    <div>
-                                        @if ($post->post_category_id != 1)
-                                        <div class="font-medium text-indigo-600 uppercase text-md dark:text-indigo-400">
-                                            {{ $post->post_category->title }}
-                                        </div>
-                                        @endif
-                                    </div>
-                                    @if ($post->is_featured)
-                                    <x-badge.post-featured>
-                                        Featured
-                                    </x-badge.post-featured>
-                                    @endif
-                                </div>
-                                @endif
-                                <p class="text-lg font-bold text-gray-900 dark:text-gray-100">
-                                    {{ $post->title }}
-                                </p>
-                                @if ($post->excerpt)
-                                <p class="mt-1 text-gray-500 text-md dark:text-gray-400">
-                                    {{ $post->excerpt }}
-                                </p>
-                                @else
-                                <p class="mt-1 text-gray-500 text-md dark:text-gray-400">
-                                    {{ Str::limit(strip_tags($post->body), 100) }}
-                                </p>
-                                @endif
-                            </div>
-                            <div class="flex flex-col">
-                                <div class="flex flex-col justify-between sm:flex-row 2xl:flex-col">
-                                    <div class="flex items-center gap-1 mt-1">
-                                        <p class="text-gray-900 text-md dark:text-gray-100">
-                                            {{ $post->user->name }}
-                                        </p>
-                                        @if ($post->user->is_verified)
-                                        <x-badge.verified />
-                                        @endif
-                                    </div>
-                                    <div class="flex gap-2 place-items-end shrink-0">
-                                        <p class="mt-1 text-xs text-gray-900 dark:text-gray-100">
-                                            {{ $post->published_at->diffForHumans() }}
-                                        </p>
-                                        <p class="mt-1 text-xs text-gray-900 dark:text-gray-100">
-                                            {{ $post->views }}
-                                            {{ Str::plural('view', $post->views) }}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </x-card.post-public>
-                @empty
-                <div class="p-5 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-500">
-                    <p class="text-gray-500 text-md dark:text-gray-400">
-                        {{ __('No post found!') }}
-                    </p>
-                </div>
-                @endforelse
-            </div> --}}
         </x-section>
         @if ($posts->hasPages())
         <x-section>
