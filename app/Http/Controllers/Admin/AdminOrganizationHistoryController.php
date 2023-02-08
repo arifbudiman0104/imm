fac0ba87871c5671953c85b0793937e7b2f3b986
arifbudimanar <arifbudiman01041996@hotmail.com>
2023-02-08 19:00:10 +0700

    add delete org his dashboard and admin

--- app/Http/Controllers/Admin/AdminOrganizationHistoryController.php
+++ app/Http/Controllers/Admin/AdminOrganizationHistoryController.php
@@ -2,90 +2,80 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Organization;
 use Illuminate\Http\Request;
 use App\Models\OrganizationHistory;
 use App\Http\Controllers\Controller;
+use App\Models\OrganizationPosition;
 use Illuminate\Support\Facades\Gate;
+use App\Models\OrganizationField;
 
 class AdminOrganizationHistoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         Gate::authorize('admin') || Gate::authorize('superadmin');
         $organization_histories = OrganizationHistory::with('user')
-        ->where('user_id', '!=', '1')
-        ->orderBy('user_id', 'asc')
-        ->paginate(21);
+            ->where('user_id', '!=', '1')
+            ->orderBy('user_id', 'asc')
+            ->paginate(21);
         return view('admin.organization-histories.index', compact('organization_histories'));
         // return $organization_histories;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\OrganizationHistory  $organizationHistory
-     * @return \Illuminate\Http\Response
-     */
     public function show(OrganizationHistory $organizationHistory)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\OrganizationHistory  $organizationHistory
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(OrganizationHistory $organizationHistory)
+    public function edit(OrganizationHistory $organization_history)
     {
-        //
+        $organizations = Organization::all();
+        $organization_positions = OrganizationPosition::all();
+        $organization_fields = OrganizationField::all();
+        return view(
+            'admin.organization-histories.edit',
+            compact(
+                'organization_history',
+                'organizations',
+                'organization_positions',
+                'organization_fields'
+            )
+        );
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\OrganizationHistory  $organizationHistory
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, OrganizationHistory $organizationHistory)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'organization_id' => 'required',
+            'organization_position_id' => 'required',
+            'organization_field_id' => 'required',
+            'start_year' => 'required',
+            'end_year' => 'required'
+        ]);
+
+        $organization_history = OrganizationHistory::find($id);
+        $organization_history->organization_id = $request->organization_id;
+        $organization_history->organization_position_id = $request->organization_position_id;
+        $organization_history->organization_field_id = $request->organization_field_id;
+        $organization_history->start_year = $request->start_year;
+        $organization_history->end_year = $request->end_year;
+        $organization_history->save();
+
+        return redirect()->route('dashboard.organization-histories.index')
+            ->with('success', 'Organization History Updated Successfully!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\OrganizationHistory  $organizationHistory
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(OrganizationHistory $organizationHistory)
     {
         Gate::authorize('admin') || Gate::authorize('superadmin');
--- app/Http/Controllers/Dashboard/DashboardOrganizationHistoryController.php
+++ app/Http/Controllers/Dashboard/DashboardOrganizationHistoryController.php
@@ -2,23 +2,15 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Models\User;
 use App\Models\Organization;
 use Illuminate\Http\Request;
 use App\Models\OrganizationField;
 use App\Models\OrganizationHistory;
 use App\Http\Controllers\Controller;
 use App\Models\OrganizationPosition;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Gate;
 
 class DashboardOrganizationHistoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $organization_histories = OrganizationHistory::with('organization', 'organization_position', 'organization_field')
@@ -28,11 +20,6 @@ class DashboardOrganizationHistoryController extends Controller
         return view('dashboard.organization-histories.index', compact('organization_histories'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         $organizations = Organization::all();
@@ -41,12 +28,6 @@ class DashboardOrganizationHistoryController extends Controller
         return view('dashboard.organization-histories.create', compact('organizations', 'organization_positions', 'organization_fields'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -67,50 +48,29 @@ class DashboardOrganizationHistoryController extends Controller
         $organization_history->save();
 
         return redirect()->route('dashboard.organization-histories.index')
-            // ->with('status', 'organization-history-created');
             ->with('success', 'Organization History Created Successfully!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(OrganizationHistory $organization_history)
     {
-        $organizations = Organization::all();
-        $organization_positions = OrganizationPosition::all();
-        $organization_fields = OrganizationField::all();
-        return view(
-            'dashboard.organization-histories.edit',
-            compact(
-                'organization_history',
-                'organizations',
-                'organization_positions',
-                'organization_fields'
-            )
-        );
+        if ($organization_history->user_id == auth()->user()->id) {
+            $organizations = Organization::all();
+            $organization_positions = OrganizationPosition::all();
+            $organization_fields = OrganizationField::all();
+            return view(
+                'dashboard.organization-histories.edit',
+                compact('organization_history', 'organizations', 'organization_positions', 'organization_fields')
+            );
+        } else {
+            return back()->with('warning', 'You are not authorized to edit this organization history!');
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $request->validate([
@@ -133,12 +93,6 @@ class DashboardOrganizationHistoryController extends Controller
             ->with('success', 'Organization History Updated Successfully!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $organization_history = OrganizationHistory::find($id);
@@ -148,7 +102,7 @@ class DashboardOrganizationHistoryController extends Controller
                 ->with('success', 'Organization History Deleted Successfully!');
         } else {
             return redirect()->route('dashboard.organization-histories.index')
-                ->with('danger', 'You are not authorized to delete this organization history!');
+                ->with('warning', 'You are not authorized to delete this organization history!');
         }
     }
 }
